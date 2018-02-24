361a131f0ef6518cebb8fb783cbd25c14a3bd23c
jona62 <jonathanjamesm66@gmail.com>
2018-02-24 00:57:17 -0500

    update

--- Iteration_1/Iteration_1.cpp
+++ Iteration_1/Iteration_1.cpp
@@ -109,14 +109,23 @@ int count(int x){
   return count;
 }
 
+int fibonacci(int n){
+  if(n == 1 || n == 2)
+    return 1;
+  else{
+    return fibonacci(n-1) + fibonacci(n-2);
+  }
+}
+
 int main() {
     int array[] ={3,6,9,12,15,18,21,24,27,30,33,36,39,42,45,48,51,54,57,60,63,66,69,72,75,78,81,84,87,90,93,96,99,102,105,108,111,114,117,120,123,126,129,132,135,138,141,144,147,150};
 
   thePromise();
   divisbleBy3();
   factorsOf(59);
-  
+
   cout<< multiply(-2,-2);
   cout<<count(11221);
+  cout<<fibonacci(4);
 return 0;
 }
