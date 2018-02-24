1169708c2e9784cca4514b939bc35d218179a47d
Jonathan James Mshelia <jonathanjamesm66@gmail.com>
2018-02-23 23:31:49 -0500

    Added a few descriptions

--- Iteration_1/Iteration_1.cpp
+++ Iteration_1/Iteration_1.cpp
@@ -57,7 +57,7 @@ if(y < 0) y = y * (-1);
 
 }
 
-//3.
+//3. Multiplies two integers without using the '*' operator, works for negative numbers as well
 int multiply(int x, int y){
   int answer = 0;
 if((x < 0) && (y < 0)){
@@ -112,10 +112,11 @@ int count(int x){
 int main() {
     int array[] ={3,6,9,12,15,18,21,24,27,30,33,36,39,42,45,48,51,54,57,60,63,66,69,72,75,78,81,84,87,90,93,96,99,102,105,108,111,114,117,120,123,126,129,132,135,138,141,144,147,150};
 
-  // thePromise();
-  // divisbleBy3();
-//  factorsOf(59);
-//   cout<< multiply(-2,-2);
-//   cout<<count(11221);
+  thePromise();
+  divisbleBy3();
+  factorsOf(59);
+  
+  cout<< multiply(-2,-2);
+  cout<<count(11221);
 return 0;
 }
