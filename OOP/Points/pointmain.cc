284b813dcedf972ceb888f5a03e473c5d774f267
jona62 <jonathanjamesm66@gmail.com>
2018-03-08 23:12:01 -0500

    Added Inheritance for the Point class

--- OOP/Points/pointmain.cc
+++ OOP/Points/pointmain.cc
@@ -4,10 +4,15 @@
 
 int main(){
   Point p1(1.0, 1.9);
-  std::cout<<"\n("<<p1.get_x()<<", "<<p1.get_y()<<")\n";
+  std::cout<<"\nPoint : ("<<p1.get_x()<<", "<<p1.get_y()<<")\n";
 
    p1.shift(1.0,1.0);
-   std::cout<<"("<<p1.get_x()<<", "<<p1.get_y()<<")\n";
+   std::cout<<"Point after a shift : ("<<p1.get_x()<<", "<<p1.get_y()<<")\n";
+
+  Circle cir(11,23);
+
+  std::cout<<"Circle center : ("<<cir.get_x()<<", "<<cir.get_y()<<") \n";
+  std::cout<<"Radius : "<<cir.get_radius()<<"\n";
 
  return 0;
 }
