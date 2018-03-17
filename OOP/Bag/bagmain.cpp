10864a5c07c4a0b0b51ce80ceb2f7c6b99c52476
jona62 <jonathanjamesm66@gmail.com>
2018-03-17 18:08:24 -0400

    Updates to Main.cpp

--- OOP/Bag/bagmain.cpp
+++ OOP/Bag/bagmain.cpp
@@ -3,26 +3,20 @@
 int main(){
   Bag bag1;
 
-  cout<<"All zeros: \n";
-  bag1.print();
-  cout<<endl;
-
   cout<<"Random Numbers: \n";
   bag1.fillinArray();
   bag1.print();
   cout<<endl;
 
-  cout<<"Modified: \n";
+  cout<<"Modified Array: \n";
   bag1.remove(0);
   bag1.remove(0);
   bag1.remove(0);
   bag1.print();
   cout<<endl;
 
-  cout<<"Deleted Array: \n";
+  cout<<"Deletes Array \n";
   bag1.deleteArray();
-  bag1.print();
-  cout<<endl;
 
   return 0;
 }
