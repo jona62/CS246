598f8c70ac4110e06b14b6c777981fd2ee547da6
jona62 <jonathanjamesm66@gmail.com>
2018-03-25 02:15:48 -0400

    Bank class Update

--- OOP/BankAccount/bank.cc
+++ OOP/BankAccount/bank.cc
@@ -1,11 +1,13 @@
-<<<<<<< HEAD
+#include "bank.h"
 Bank::Bank(){
 	balance = 0.0;
+	set_acct_No();
 	acct_No = get_acct_No();
 }
+
 void Bank::set_acct_No(){
 	srand(time(NULL));
-	int pass = rand()% 2000 + 20000;
+	int pass = rand()% 900000000 + 200000000;
 	acct_No = pass;
 }
 
@@ -17,7 +19,10 @@ void Bank::withdraw(int amount){
 	balance -= amount;
 }
 
-void Bank::set_name(string str){
+void Bank::set_name(){
+	string str;
+	cout<<"Enter Your Fullname: ";
+	getline(cin, name);
 	name = str;
 }
 
@@ -33,7 +38,7 @@ string Bank::get_name() const{
 	return name;
 }
 
-void Bank::display_info(){
+void Bank::display_menu(){
   int choice;
   int amount;
   cout<<"\n\n";
@@ -50,7 +55,7 @@ void Bank::display_info(){
       cout<<"Enter amount: ";
       cin>>amount;
       Bank::deposit(amount);
-      Bank::display_info();
+      Bank::display_menu();
     break;
     case 2:
       do{cout<<"Enter amount: ";
@@ -58,95 +63,25 @@ void Bank::display_info(){
       if(Bank::get_balance() - amount < 0) cout<<"\nYou know thats not possible bruh!\nEnter what you have";
       }while(Bank::get_balance() - amount < 0);
       Bank::withdraw(amount);
-      Bank::display_info();
+      Bank::display_menu();
     break;
     case 3:
+			cout<<"\n******************************************************************\n";
       cout<<"Your Balance is $"<<Bank::get_balance();
-      Bank::display_info();
+      Bank::display_menu();
     break;
     case 4:
     break;
 
     default:
       cout<<"Invalid! Operation Try again ";
-      Bank::display_info();
+      Bank::display_menu();
   }
 
-}
-=======
-#include "bank"
-Bank::Bank(){
-	balance = 0.0;
-	acct_No = get_acct_No();
-}
-void Bank::set_acct_No(){
-	srand(time(NULL));
-	int pass = rand()% 2000 + 20000;
-	acct_No = pass;
-}
-
-void Bank::deposit(int amount){
-	balance += amount;
-}
-
-void Bank::withdraw(int amount){
-	balance -= amount;
-}
-
-void Bank::set_name(string str){
-	name = str;
-}
-
-int Bank::get_balance() const{
-	return balance;
-}
-
-int Bank::get_acct_No() const{
-	return acct_No;
-}
-
-string Bank::get_name() const{
-	return name;
 }
 
 void Bank::display_info(){
-  int choice;
-  int amount;
-  cout<<"\n\n";
-  stringstream out;
-  cout<<"******************************************************************\n";
-  out<<left<<"1.Cash Deposit"<<setw(40)<<right<<"2.Cash Withdrawal\n\n";
-  out<<left<<"3.Balance Enquiry"<<setw(26)<<right<<"4.Exit\n\n";
-  out<<"Select your Transaction: ";
-  cout<<out.str();
-
-  cin>>choice;
-  switch (choice){
-    case 1:
-      cout<<"Enter amount: ";
-      cin>>amount;
-      Bank::deposit(amount);
-      Bank::display_info();
-    break;
-    case 2:
-      do{cout<<"Enter amount: ";
-      cin>>amount;
-      if(Bank::get_balance() - amount < 0) cout<<"\nYou know thats not possible bruh!\nEnter what you have";
-      }while(Bank::get_balance() - amount < 0);
-      Bank::withdraw(amount);
-      Bank::display_info();
-    break;
-    case 3:
-      cout<<"Your Balance is $"<<Bank::get_balance();
-      Bank::display_info();
-    break;
-    case 4:
-    break;
-
-    default:
-      cout<<"Invalid! Operation Try again ";
-      Bank::display_info();
-  }
-
+	cout<<"Welcome "<<get_name();
+	cout<<"\nYour Account Number is: "<<get_acct_No();
+	cout<<", and you have $"<<get_balance()<<" in your account. \n";
 }
->>>>>>> b2346603408c95fc615123dd29f28c6ef50b61bf
